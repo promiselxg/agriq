78e90a83a53197e55c736735ac32859f214305ae
promiselxg <okeydeede@gmail.com>
2023-12-13 12:11:03 +0100

    added slider

--- src/app/_components/Hero/Hero.jsx
+++ src/app/_components/Hero/Hero.jsx
@@ -1,79 +1,71 @@
+"use client";
+import Image from "next/image";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+
 const Hero = () => {
   return (
     <>
       <div className="w-full relative ">
-        <div className="hero-bg md:h-screen h-[200px] ">
-          <div className="absolute top-0 bottom-0 bg-[rgba(0,0,0,0.3)] w-full md:h-full"></div>
-          <div className="md:w-[1200px] mx-auto flex items-center md:h-screen h-[200px]">
-            <div className="md:container flex  justify-between items-center gap-6">
-              <div className="w-3/4 max-w-max flex flex-initial md:flex-col ml-[20px] bg-[rgba(0,0,0,0.5)] p-10 ">
-                <h1 className="text-sm md:text-5xl font-extrabold text-[#fff] z-10 leading-tight mb-4">
-                  How agricultural innovation &amp; sustainability impact
-                  harvest.
+        <div className="hero-bg md:h-[700px] h-[300px] ">
+          <Carousel
+            autoPlay
+            infiniteLoop={true}
+            transitionTime={500}
+            showStatus={false}
+            interval={5000}
+            emulateTouch
+            dynamicHeight={true}
+            showThumbs={false}
+          >
+            <div className="relative">
+              <Image
+                src="/sliders/3.jpg"
+                width={1200}
+                height={700}
+                className="h-[300px] md:h-[700px]"
+                alt="image"
+              />
+              <div className="w-3/4 max-w-max flex flex-initial md:flex-col ml-[50px] bg-[rgba(0,0,0,0.5)] md:p-10 p-5 absolute md:top-[200px] top-[60px]">
+                <h1 className="text-sm md:text-3xl font-extrabold text-[#fff] z-10 leading-tight mb-4 justify-start text-left capitalize">
+                  PIONEERED GREEN HOUSE TOMATO PRODUCTION WITH DRIP IRRIGATION
+                  IN OGUN STATE
                 </h1>
               </div>
-              {/* <div className="w-1/2 flex items-center justify-end">
-                <div className="w-full">
-                  <div className="w-full gap-5 relative flex flex-wrap justify-center">
-                    <div className="w-fit services_box bg-[#fff] rounded-[50%] p-3 overflow-hidden rotate cursor-pointer">
-                      <Image
-                        src="/services/Agribusiness.png"
-                        width={100}
-                        height={100}
-                        alt="Agribusiness"
-                        className="w-fit h-[100px]"
-                      />
-                    </div>
-                    <div className="w-fit services_box bg-[#fff] rounded-[50%] p-3 overflow-hidden rotate cursor-pointer">
-                      <Image
-                        src="/services/Training.png"
-                        width={100}
-                        height={100}
-                        alt="Training"
-                        className="w-fit h-[100px]"
-                      />
-                    </div>
-                    <div className="w-fit services_box bg-[#fff] rounded-[50%] p-3 overflow-hidden rotate cursor-pointer">
-                      <Image
-                        src="/services/implementation.png"
-                        width={100}
-                        height={100}
-                        alt="implementation"
-                        className="w-fit h-[100px]"
-                      />
-                    </div>
-                    <div className="w-fit services_box bg-[#fff] rounded-[50%] p-3 overflow-hidden rotate cursor-pointer">
-                      <Image
-                        src="/services/Advisory.png"
-                        width={100}
-                        height={100}
-                        alt="Advisory"
-                        className="w-fit h-[100px]"
-                      />
-                    </div>
-                    <div className="w-fit services_box bg-[#fff] rounded-[50%] p-3 overflow-hidden rotate cursor-pointer">
-                      <Image
-                        src="/services/Market_access.png"
-                        width={100}
-                        height={100}
-                        alt="Market access"
-                        className="w-fit h-[100px]"
-                      />
-                    </div>
-                    <div className="w-fit services_box bg-[#fff] rounded-[50%] p-3 overflow-hidden rotate cursor-pointer">
-                      <Image
-                        src="/services/Farm_Operation.png"
-                        width={100}
-                        height={100}
-                        alt="Farm Operation"
-                        className="w-fit h-[100px]"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div> */}
             </div>
-          </div>
+            <div className="relative">
+              <Image
+                src="/sliders/1.jpg"
+                width={1200}
+                height={700}
+                className="h-[300px] md:h-[700px]"
+                alt="image"
+              />
+              <div className="w-3/4 max-w-max flex flex-initial md:flex-col ml-[50px] bg-[rgba(0,0,0,0.5)] md:p-10 p-5 absolute md:top-[200px] top-[60px]">
+                <h1 className="text-sm md:text-3xl font-extrabold text-[#fff] z-10 leading-tight mb-4 justify-start text-left capitalize">
+                  INCREASED RICE YIELD BY MORE THAN 400% USING SMART
+                  AGRIBUSINESS SOLUTIONS DELIVERED INTELLIGENCE IN AGRIBUSINESS
+                  THROUGH AGRIQ LEARNING ACADEMY
+                </h1>
+              </div>
+            </div>
+            <div className="relative">
+              <Image
+                src="/sliders/2.jpg"
+                width={1200}
+                height={700}
+                className="h-[300px] md:h-[700px]"
+                alt="image"
+              />
+              <div className="w-3/4 max-w-max flex flex-initial md:flex-col ml-[50px] bg-[rgba(0,0,0,0.5)] md:p-10 p-5 absolute md:top-[200px] top-[60px]">
+                <h1 className="text-sm md:text-3xl font-extrabold text-[#fff] z-10 leading-tight mb-4 justify-start text-left capitalize">
+                  INCREASED CASSAVA YIELD PER HECTARE BY MORE THAN 350% USING
+                  SMART AGRIBUSINESS SOLUTIONS
+                </h1>
+              </div>
+            </div>
+          </Carousel>
         </div>
       </div>
     </>
